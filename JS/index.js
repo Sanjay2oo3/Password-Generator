b9c605674a45a870c56c6563457c52e5c36d30da
Sanjay Choudhary <itssanjaychoudhary423@gmail.com>
2024-02-14 00:20:50 +0530

    Regenrate Password &  bugs

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-document.body.style.zoom="90%"
+document.body.style.zoom = "90%"
 
 const lightMode = document.getElementById("lightmode");
 const darkMode = document.getElementById("darkmode");
@@ -6,7 +6,7 @@ const mode = document.getElementById("theme")
 const text = document.getElementsByClassName("text")
 const inputBackground = document.getElementsByClassName("inputbg")
 const borderClr = document.getElementsByClassName("borderclr")
-const refBtn =document.getElementsByClassName("btnclr")
+const refBtn = document.getElementsByClassName("btnclr")
 const copyBtn = document.getElementById('cpybtn')
 const displayPass = document.getElementById("password")
 const upCase = document.getElementById("ucase")
@@ -17,12 +17,12 @@ const regenBtn = document.getElementById("regen")
 const passLengthinput = document.getElementById("passlen")
 const passRange = document.getElementById("passlenrgn")
 
-passLengthinput.oninput = function(){ 
+passLengthinput.oninput = function () {
     passRange.value = passLengthinput.value;
     generatePassword(arr, passLengthinput.value);
 }
 
-passRange.oninput = function(){
+passRange.oninput = function () {
     passLengthinput.value = passRange.value;
     generatePassword(arr, passLengthinput.value);
 }
@@ -31,122 +31,122 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+";
+const arr = [];
 
-mode.addEventListener("click", function changeTheme(){
+
+function generatePassword(arr, length) {
+    let str = arr.join('');
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += str.charAt(Math.floor(Math.random() * str.length));
+    }
+    displayPass.value = password;
+}
+
+function addToArray(characters) {
+    arr.push(characters);
+    generatePassword(arr, passLengthinput.value);
+}
+
+function onCheckEvent(checker, characters) {
+    if (checker.checked) {
+        addToArray(characters);
+    } else {
+        arr.splice(arr.indexOf(characters), 1);
+        generatePassword(arr, passLengthinput.value);
+    }
+}
+
+regenBtn.addEventListener("click", function () {
+    generatePassword(arr, passLengthinput.value);
+})
+
+mode.addEventListener("click", function changeTheme() {
 
     const BgColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
 
-    if(BgColor === '#ffffff' || BgColor === 'rgb(255, 255, 255)'){
-        
-        document.body.style.backgroundColor="black";
+    if (BgColor === '#ffffff' || BgColor === 'rgb(255, 255, 255)') {
+
+        document.body.style.backgroundColor = "black";
 
         darkMode.classList.add("hidden")
         lightMode.classList.remove("hidden")
-        
+
         Array.from(text).forEach(element => {
             element.classList.add("text-white");
-          });
+        });
 
         Array.from(inputBackground).forEach(element => {
             element.classList.add("bg-black");
             console.log(element)
-          });
-          borderClr[0].classList.add("border-white")
-          refBtn[0].classList.remove("bg-custom-black")
-          refBtn[0].classList.remove("text-white")
-          refBtn[0].classList.add("bg-gray-100")
+        });
+        borderClr[0].classList.add("border-white")
+        refBtn[0].classList.remove("bg-custom-black")
+        refBtn[0].classList.remove("text-white")
+        refBtn[0].classList.add("bg-gray-100")
 
 
-    }else
-    {
-        document.body.style.backgroundColor="white";
+    } else {
+        document.body.style.backgroundColor = "white";
 
         darkMode.classList.remove("hidden")
         lightMode.classList.add("hidden")
 
         Array.from(text).forEach(element => {
             element.classList.remove("text-white");
-          });
+        });
 
-          Array.from(inputBackground).forEach(element => {
+        Array.from(inputBackground).forEach(element => {
             element.classList.remove("bg-black");
-          });
-          borderClr[0].classList.remove("border-white")
-          refBtn[0].classList.add("bg-custom-black")
-          refBtn[0].classList.add("text-white")
-          refBtn[0].classList.remove("bg-gray-100")
+        });
+        borderClr[0].classList.remove("border-white")
+        refBtn[0].classList.add("bg-custom-black")
+        refBtn[0].classList.add("text-white")
+        refBtn[0].classList.remove("bg-gray-100")
 
     }
 })
 
-// copyBtn.addEventListener("click", function copy(){
-//     displayPass.select();
-//     document.execCommand("copy");
-// })
-
-copyBtn.addEventListener("click", function() {
+copyBtn.addEventListener("click", function () {
     displayPass.select();
     navigator.clipboard.writeText(displayPass.value)
-        .then(function() {
+        .then(function () {
             console.log("Text successfully copied to clipboard");
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.error("Unable to copy text to clipboard", err);
         });
 });
 
-const arr=[];
-
-upCase.addEventListener("change", function(){
-    if(upCase.checked){
-        arr.push(upperCase);
-        generatePassword(arr, passLengthinput.value);
-    }else
-    {
-        arr.splice(arr.indexOf(upperCase),1);
-        generatePassword(arr, passLengthinput.value);
-    }
+upCase.addEventListener("change", function () {
+    onCheckEvent(upCase, upperCase);
 })
 
-lowCase.addEventListener("change", function(){
-    if(lowCase.checked){
-        arr.push(lowerCase);
-        generatePassword(arr, passLengthinput.value);
-    }else
-    {
-        arr.splice(arr.indexOf(lowerCase),1);
-        generatePassword(arr, passLengthinput.value);
-    }
+lowCase.addEventListener("change", function () {
+    onCheckEvent(lowCase, lowerCase);
 })
 
-numCase.addEventListener("change", function(){
-    if(numCase.checked){
-        arr.push(numbers);
-        generatePassword(arr, passLengthinput.value);
-    }else
-    {
-        arr.splice(arr.indexOf(numbers),1);
-        generatePassword(arr, passLengthinput.value);
-    }
+numCase.addEventListener("change", function () {
+    onCheckEvent(numCase, numbers);
 })
 
-symCase.addEventListener("change", function(){
-    if(symCase.checked){
-        arr.push(symbols);
-        generatePassword(arr, passLengthinput.value);
-    }else
-    {
-        arr.splice(arr.indexOf(symbols),1);
-        generatePassword(arr, passLengthinput.value);
-    }
+symCase.addEventListener("change", function () {
+    onCheckEvent(symCase, symbols);
 })
 
-function generatePassword(arr, length){
-    let str = arr.join('');
 
-    let password = "";
-    for(let i=0; i<length; i++){
-        password += str.charAt(Math.floor(Math.random() * str.length));
-    }
-    displayPass.value = password;
+if (upCase.checked) {
+    addToArray(upperCase);
 }
+if (lowCase.checked) {
+    addToArray(lowerCase);
+}
+if (numCase.checked) {
+    addToArray(numbers);
+}
+if (symCase.checked) {
+    addToArray(symbols);
+}
+
+
